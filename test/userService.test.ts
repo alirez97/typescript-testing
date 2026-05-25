c52b23fc19ea47b0b92a7dc48c76231324ed1765
Alireza <khanamani1998@gmail.com>
2026-05-25 22:37:30 +0330

    feat: add test suit for getUserCount function.

--- test/userService.test.ts
+++ test/userService.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it, vi } from 'vitest';
-import { fetchUser, User } from '../src/api';
-import { getUserName } from '../src/userService';
+import { fetchUser, fetchUsers, User } from '../src/api';
+import { getUserCount, getUserName } from '../src/userService';
 
 vi.mock('../src/api');
 
@@ -13,5 +13,20 @@ describe('getUserName', () => {
     const name = await getUserName(1);
 
     expect(name).toBe(mockUser.name);
+  });
+});
+
+describe('getUserCount', () => {
+  it('should return number of users', async () => {
+    const mockUsers: User[] = [
+      { id: 1, name: 'Alireza', email: 'alireza@gmail.com' },
+      { id: 2, name: 'Sara', email: 'sara@gmail.com' },
+    ];
+
+    vi.mocked(fetchUsers).mockResolvedValue(mockUsers);
+
+    const count = await getUserCount();
+
+    expect(count).toBe(mockUsers.length);
   });
 });
